e60011695c0c242533d92f0865d2cb2a101be890
Matthew Pandolfo <matt@pandolfo.com>
2021-08-30 17:37:36 -0400

    buttons for different file types

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 function getUserRepos(user) {
   // format the guthub api url
@@ -86,4 +87,27 @@ function displayRepos(repos, searchTerm) {
   }
 }
 
+function getFeaturedRepos(language) {
+  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+
+  fetch(apiUrl).then(function(response) {
+    if(response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+} 
+
+function buttonClickHandler(event) {
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+    repoContainerEl.textContent = '';
+  }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener('click', buttonClickHandler);
